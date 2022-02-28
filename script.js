b98ae08d90abc4a37933b71aea80375a81d51c64
Harry Katsaris <mrkatsaris@gmail.com>
2022-02-28 19:14:48 +0100

    added accesibility and rensponsive Navigation

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const navLinks = document.querySelector('.nav-links');
 
 navBurger.addEventListener('click', () => {
    navLinks.classList.toggle('nav-links');
+   navLinks.classList.toggle('active');
 
-   navBurger.classList.toggle('nav-burger');
-})
+ //navLinks.classList.remove('active');    
+ //navBurger.classList.toggle('active');
+}) 
+
+/*const toggleNav = () => {
+   navLinks.classList.toggle('nav-links');
+   navLinks.classList.add("active");
+}
+
+navBurger.addEventListener('click', toggleNav); */
 
